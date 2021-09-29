2933f2b58097683040ec34a4ff1e746a633006ad
gabriel-5 <82604791+gabriel-5@users.noreply.github.com>
2021-09-29 09:20:25 +0100

    Add miner

--- index.js
+++ index.js
@@ -29,18 +29,27 @@ class Room {
         this._description = description;
         this._linkedRooms = {};
         this._linkedItems = {};
+        this._linkedChars = {};
     }
 
     get linkedRooms() {
         return this._linkedRooms;
     }
 
-    linkRoom(direction, room) {
-        this._linkedRooms[direction] = room;
-    }
-
     get linkedItems() {
         return this._linkedItems;
+    }
+
+    get linkedChars() {
+        return this._linkedChars;
+    }
+
+    linkChar(x, y) {
+        this._linkedChars[x] = y;
+    }
+
+    linkRoom(direction, room) {
+        this._linkedRooms[direction] = room;
     }
 
     linkItem(desc, item) {
@@ -166,7 +175,7 @@ class Character {
     }
 
     talk() {
-        return `${this._name} says: ${this._conversaion}`;
+        return `${this._name} appears and says: ${this._conversaion}`;
     }
 
     constructor(name, description, conversation) {
@@ -176,32 +185,12 @@ class Character {
     }
 }
 
-class Enemy extends Character {
-    constructor(name, weakness) {
-        super(name, "Enemy");
-        this._weakness = weakness;
-    }
-
-    fight(item) {
-        if (item == this._weakness) {
-            return true;
-
-        } else {
-            return false;
-        }
-    }
-
-    attack() {
-        alert(`${this._name} attacks you!`)
-    }
-}
-
-const Silas = new Character("Silas", "A small bronze statue of a dog, glued back together.", "Thank you for putting me back together. I've been like that for years. Ever since the last person arrived here.")
+const Silas = new Character("Silas", "An elderly worker in the foundry.", "I see you took the note... I wouldn't advise going visiting the graveyard with that in your pocket. They won't be happy about it.")
 
 
-const Kitchen = new Room("kitchen", "You are in a kitchen. It is spotless and unbearably humid, like a greenhouse in summer. There is an unplugged fridge-freezer with a note on the front which reads 'Welcome to Skidded Hollow. Please do not remove this note'. To the north, there is a metal door. To the east, a set of swinging saloon doors. To the south, a red door with a circle carved into it. To the west, a tunnel you may be able to crawl through");
-const Foundry = new Room("foundry", "The must be where all the heat was coming from. In what looks like a foundry of some sort, there are eight men all all melting down what looks like bronze in a home-made crucible. No matter what you try, they do not acknowledge you. The door to the south leads back to the kitchen");
-const ProcessingPlant = new Room("processing plant", "You are in a room full of big tanks processing unidentifiable thick liquids. The place smells like a combination of shoe polish and strong cider. Some of the tanks have small cracks and the liquid is seeping out at a glacial pace. The leaking liquid looks like tree sap. On the floor there a is half-full tube of glue; it looks like someone has unsuccessfully tried to fix the cracks in the tubes using it. The tunnel to the east leads back to the kitchen")
+const Kitchen = new Room("kitchen", "You are in a kitchen. It is spotless and unbearably humid, like a greenhouse in summer. There is an unplugged fridge-freezer with a note on the front which reads 'Welcome to Skidded Hollow. Please do not remove this note'. On the dining table, there is a small bottle of glue. To the north, there is a metal door. To the east, a set of swinging saloon doors. To the south, a red door with a circle carved into it. To the west, a tunnel you may be able to crawl through");
+const Foundry = new Room("foundry", "The must be where all the heat was coming from. In what looks like a foundry of some sort, there are eight men all all melting down what looks like bronze in a home-made crucible. They don't seem to notice you. The door to the south leads back to the kitchen");
+const ProcessingPlant = new Room("processing plant", "You are in a room full of big tanks processing unidentifiable thick liquids. The place smells like a combination of shoe polish and strong cider. Some of the tanks have small cracks and the liquid is seeping out at a glacial pace. The leaking liquid looks like tree sap mixed. The tunnel to the east leads back to the kitchen")
 const Graveyard = new Room("graveyard", "You are in a a spacious, dimly-lit indoor graveyard. There's about three metres of space between each headstone. They all seem to be from wildly differing time periods. To the west is the door back into the kitchen")
 const WorshippingArea = new Room("worshipping area", "The room is nearly completely empty aside from a concrete alter in the far end of the room. On top of the alter there are the remains of a  bronze statue of a dog smashed into pieces. The door to the north leads back to the kitchen")
 
@@ -214,8 +203,10 @@ Kitchen.linkRoom("south", WorshippingArea);
 Kitchen.linkRoom("east", Graveyard);
 Kitchen.linkRoom("west", ProcessingPlant);
 
-ProcessingPlant.linkItem("glue", Glue);
+Kitchen.linkItem("glue", Glue);
 Kitchen.linkItem("note", Note);
+
+Foundry.linkChar("silas", Silas);
 
 Foundry.linkRoom("south", Kitchen);
 
@@ -248,16 +239,6 @@ window.onload = () => {
                 document.getElementById("inventory-title").style.display = "none";
                 document.getElementById("inventory").style.display = "none";
                 document.getElementById("information").style.display = "none";
-                document.getElementById("ending-text").innerHTML = "(Bad Ending)"
-                document.getElementById("map-end").style.display = "block";
-                document.getElementById("map4").style.display = "none";
-                document.getElementById("pre1").style.display = "none";
-                document.getElementById("pre2").style.display = "none";
-                document.getElementById("pre3").style.display = "none";
-                document.getElementById("pre4").style.display = "none";
-                document.getElementById("pre5").style.display = "none";
-
-
 
             } else if (command == "dog" && currentRoom == WorshippingArea && backpack.includes(" Glue ")) {
                 document.getElementById("game").innerHTML = "You use the tube of glue to stick the small bronze dog back together. It screams in pain, with the voice of an adult man. The dog stares you down, and says: 'Thank you so much for putting me back together. I've been like that since the last one got sent here. It's going to be okay. I'll send you home.'";
@@ -266,13 +247,14 @@ window.onload = () => {
                 document.getElementById("inventory-title").style.display = "none";
                 document.getElementById("inventory").style.display = "none";
                 document.getElementById("information").style.display = "none";
-                document.getElementById("ending-text").innerHTML = "(Good Ending)"
 
             } else if (directions.includes(command)) {
                 currentRoom = currentRoom.move(command);
                 document.getElementById("game").innerHTML = currentRoom.describe();
                 document.getElementById("command").value = "";
                 changeMap();
+                eventTrigger();
+
 
             } else if (actions.includes(command)) {
                 let item = currentRoom.take(command);
@@ -311,6 +293,13 @@ goodEnd.src = "images/door.png";
 let badEndSound = document.querySelector("#badEnd");
 let stepsSound = document.querySelector("#steps");
 
+
+function eventTrigger() {
+    if (currentRoom == Foundry && backpack.includes(" Note "))
+        document.getElementById("information").innerHTML = Silas.talk();
+}
+
+
 function changeMap() {
     if (currentRoom == Kitchen) {
         document.getElementById("map1").style.display = "block";
@@ -318,7 +307,6 @@ function changeMap() {
         document.getElementById("map3").style.display = "none";
         document.getElementById("map4").style.display = "none";
         document.getElementById("map5").style.display = "none";
-
 
         document.getElementById("pre1").style.display = "block";
         document.getElementById("pre2").style.display = "none";
@@ -332,7 +320,6 @@ function changeMap() {
         document.getElementById("map4").style.display = "none";
         document.getElementById("map5").style.display = "none";
 
-
         document.getElementById("pre1").style.display = "none";
         document.getElementById("pre2").style.display = "block";
         document.getElementById("pre3").style.display = "none";
@@ -344,7 +331,6 @@ function changeMap() {
         document.getElementById("map3").style.display = "block";
         document.getElementById("map4").style.display = "none";
         document.getElementById("map5").style.display = "none";
-
 
         document.getElementById("pre1").style.display = "none";
         document.getElementById("pre2").style.display = "none";
@@ -359,7 +345,6 @@ function changeMap() {
         document.getElementById("map4").style.display = "block";
         document.getElementById("map5").style.display = "none";
 
-
         document.getElementById("pre1").style.display = "none";
         document.getElementById("pre2").style.display = "none";
         document.getElementById("pre3").style.display = "none";
@@ -372,12 +357,11 @@ function changeMap() {
         document.getElementById("map4").style.display = "none";
         document.getElementById("map5").style.display = "block";
 
-
         document.getElementById("pre1").style.display = "none";
         document.getElementById("pre2").style.display = "none";
         document.getElementById("pre3").style.display = "none";
         document.getElementById("pre4").style.display = "none";
         document.getElementById("pre5").style.display = "block";
-
     }
 }
+
